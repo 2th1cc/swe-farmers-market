5be5f093b4fc11b3c3fff83cf857409b77a096b4
tugraline <tugralinerzan@gmail.com>
2024-11-30 17:07:03 +0500

    Update urls.py

--- farmer_market/products/urls.py
+++ farmer_market/products/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('api/farmer/products/<int:product_id>/images/', FarmerProductImageCreateAPIView.as_view(), name='farmer_product_image_create'),
     path('api/farmer/notifications/low-stock/', LowStockNotificationListAPIView.as_view(), name='low_stock_notifications'),
     path('api/products/', ProductListAPIView.as_view(), name='product_list'),
+
+    path('api/token/', views.obtain_auth_token, name='obtain_auth_token'),
 ]
