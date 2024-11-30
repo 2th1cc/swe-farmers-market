f9824d72f2d8f0ec041b877babc9a738f0457959
Lazzat Zhengissova <lyazzat.zhenisova03@gmail.com>
2024-11-30 09:40:30 +0500

    Update serializers.py

--- farmer_market/orders/serializers.py
+++ farmer_market/orders/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from .models import DeliveryMethod
 
 class DeliveryMethodSerializer(serializers.ModelSerializer):
+    type_display = serializers.CharField(source='get_type_display', read_only=True)  # Human-readable type
+
     class Meta:
         model = DeliveryMethod
-        fields = ['id', 'name', 'description']
+        fields = ['id', 'name', 'description', 'type', 'type_display']
 
